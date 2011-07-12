bd91b9b7afa313277e4481f7aed7040ede376bc1
davidesil.web@gmail.com <davidesil.web@gmail.com@da8a94e3-c7c2-515b-8a0f-90127841e8c2>
2011-07-12 14:53:24 +0000

    errore grave

--- trunk/src/fairLock/Condition.java
+++ trunk/src/fairLock/Condition.java
@@ -19,8 +19,7 @@ public class Condition {
 		element.P();
 		queue.remove(0);
 		System.out.println(Thread.currentThread() + ": sbloccato sulla condition.");
-		//Passaggio del testimone ,senza rilasciare il lock, quindi non serve che lo riacquisisco, 
-		//qualcuno me lo avrˆ passato.
+		// Passaggio del testimone, quindi lo svegliante mi avrˆ passato il lock. 
 	}
 
 	public void signal() throws InterruptedException, IllegalUseOfConditionException {
@@ -31,14 +30,15 @@ public class Condition {
 		}
 		Semaphore urgent_element = new Semaphore();
 		Semaphore element = queue.get(0);
+		lock.addUrgentElement(urgent_element); // L'inserimento DEVE essere fatto prima della V in modo che se 
+		// il thread associato ad element parte ed arriva a fare la unlock, 
+		// quando termina mi ripassa la mutua esclusione.
 		element.V(); // signal & continue
-		lock.addUrgentElement(urgent_element);
 		System.out.println(Thread.currentThread()
 				+ ": bloccato per signal & urgent.");
-		urgent_element.P(); // passaggio del testimone, senza rilasciare il lock
+		urgent_element.P(); // Passaggio del testimone, quindi non devo rilasciare/riacquisire il lock.
 		lock.removeUrgentElement(0);
 		System.out.println(Thread.currentThread() + ": sbloccato.");
-
 	}
 	
 	public boolean empty(){
